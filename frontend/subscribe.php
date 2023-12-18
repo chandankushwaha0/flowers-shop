26c24768bd91eb89564f290f2c55d053f15c3126
chandan <kushwahhachandan@gmail.com>
2023-12-18 14:07:35 +0545

    Creating index page

---
+++ frontend/subscribe.php
@@ -0,0 +1,13 @@
+
+
+<div class="subscribe">
+    <div class="container-fluid">
+        <div class="container">
+            <h2>join the colorful branch!</h2>
+            <form action="" class="text-center">
+                <input type="email" placeholder="Email Address">
+                <button class="">Subscribe</button>
+            </form>
+        </div>
+    </div>
+</div>
